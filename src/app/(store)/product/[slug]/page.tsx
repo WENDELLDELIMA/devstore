684c8775ba9e7c8f813433a6e5bf32e55692850c
Wendell lima <wendell@MacBook-Air-de-Wendell.local>
2024-02-12 19:48:40 -0300

    create `generateStaticParams`built-in do next para salvar em cache paginas especificas`

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -39,6 +39,13 @@ export async function generateMetadata({
     description: product.description,
   };
 }
+export async function generateStaticParams() {
+  const response = await api("/products/featured");
+  const products: Product[] = await response.json();
+  return products.map((product) => {
+    return { slug: product.slug };
+  });
+}
 export default async function ProductPage({ params }: ProductProps) {
   const { product } = await getProduct(params.slug);
 
