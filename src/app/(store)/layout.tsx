bf2591efb8a846e3517b6e4f9c2e0c868f11e8d0
Wendell lima <wendell@MacBook-Air-de-Wendell.local>
2024-02-12 20:11:36 -0300

    criando contexto do carrinho:CartProvider

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,14 +1,17 @@
 import Header from "@/components/header";
+import { CartProvider } from "@/contexts/cart-context";
 import { ReactNode } from "react";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div
-      className="mx-auto grid min-h-screen w-full max-w-[1600px]
+    <CartProvider>
+      <div
+        className="mx-auto grid min-h-screen w-full max-w-[1600px]
      grid-rows-[min-content_max-content] gap-5 px-8 py-8"
-    >
-      <Header />
-      {children}
-    </div>
+      >
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
